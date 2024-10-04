3bf4b7106b8cd20db7833aa0beab71f2eadeeb56
Yazington <binaryaz@gmail.com>
2024-10-03 23:17:19 -0400

    adding small bugfix

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ from api import chat
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # logger = logging.getLogger("my_logger")
 # handler = logging.StreamHandler()
