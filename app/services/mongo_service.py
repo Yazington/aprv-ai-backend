7c9bacb3bc124d9571a5a57f8e99810ecf15e4d5
Yazington <binaryaz@gmail.com>
2024-10-25 22:07:56 -0400

    fixing mongo issue

--- app/services/mongo_service.py
+++ app/services/mongo_service.py
@@ -1,4 +1,5 @@
 import pymongo
+from config.settings import settings
 from fastapi import Depends
 from gridfs import GridFS
 from motor.motor_asyncio import AsyncIOMotorClient
@@ -7,15 +8,14 @@ from odmantic import AIOEngine
 
 class MongoService:
     def __init__(self):
-        db_url = "mongodb://root:example@localhost:27017/"
         # Use AsyncIOMotorClient for asynchronous operations
-        self.client = AsyncIOMotorClient(db_url)
+        self.client = AsyncIOMotorClient(settings.mongo_url)
         self.database_name = "aprv-ai"
         self.db_async = self.client[self.database_name]
         self.engine = AIOEngine(client=self.client, database=self.database_name)
 
         # Use pymongo.MongoClient for GridFS
-        self.client_sync = pymongo.MongoClient(db_url)
+        self.client_sync = pymongo.MongoClient(settings.mongo_url)
         self.db_sync = self.client_sync[self.database_name]
         self.fs = GridFS(self.db_sync)
 
